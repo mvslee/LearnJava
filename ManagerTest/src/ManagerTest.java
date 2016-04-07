cb163d3292508724626c943eec52277c677854b9
mavis <mvslee@gmail.com>
2016-04-07 21:58:11 +0800

    Create inheritance

---
+++ ManagerTest/src/ManagerTest.java
@@ -0,0 +1,17 @@
+import com.corejava.*;
+
+/**
+ * Created by mavis on 4/7/16.
+ */
+public class ManagerTest {
+    public static void main(String[] args){
+        Employee[] staff = new Employee[3];
+        Manager boss = new Manager("Carl", 8000, 2000, 3, 12);
+        boss.setBonus(2000);
+        staff[0] = boss;
+        staff[1] = new Employee("Mike", 6000, 2005, 4, 10);
+        staff[2] = new Employee("Jack", 5000, 2011, 5, 12);
+        for (Employee e:staff)
+            System.out.println(e.getName() + " " + e.getSalary() + " hired in " +e.getHireday());
+    }
+}
